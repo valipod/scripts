800002a74ca2afa547fa9cff1a6b54d6456f632b
Valentin Dumitru <valentin.dumitru@gmail.com>
2025-11-27 14:14:31 +0200

    add mp4 copy script

---
+++ copy-ext_rom-ext_eng_mp4.sh
@@ -0,0 +1,15 @@
+for file in *.mp4
+do
+  if [ -z "$1" ];
+    then
+	  #read -t 5 -p "No arguments supplied (proceed in 5 seconds or ENTER) ..."
+      ffmpeg -i "$file" -i "${file%.mp4}.ro.srt" -i "${file%.mp4}.en.srt" -map 0:v:0 -map 0:a:0 -map 1:0 -map 2:0 -metadata:s:s:0 language=ron -metadata:s:s:1 language=eng -c copy -max_interleave_delta 0 -metadata:s:a:0 title=English "../${file%mp4}mkv"
+  elif [ -z "$2" ];
+    then
+    #read -t 5 -p "Target will be saved in $1 (proceed in 5 seconds or ENTER) ..."
+    ffmpeg -i "$file" -i "${file%.mp4}.ro.srt" -i "${file%.mp4}.en.srt" -map 0:v:0 -map 0:a:0 -metadata:s:a:0 language=eng -map 1:0 -map 2:0 -metadata:s:s:0 language=ron -metadata:s:s:1 language=eng -c copy -max_interleave_delta 0 -metadata:s:a:0 title=English "$1/${file%mp4}mkv"
+  else
+    #read -t 5 -p "Target will be saved in $1/$2 (proceed in 5 seconds or ENTER) ..."
+    ffmpeg -i "$file" -i "${file%.mp4}.ro.srt" -i "${file%.mp4}.en.srt" -map 0:v:0 -map 0:a:0 -map 1:0 -map 2:0 -metadata:s:s:0 language=ron -metadata:s:s:1 language=eng -c copy -max_interleave_delta 0 -metadata:s:a:0 title=English "$1/$2"
+  fi
+done
